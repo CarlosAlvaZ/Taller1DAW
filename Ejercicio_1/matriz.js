d28a5a60d3a6e07749b19d939b884827ca16cdbf
CarlosAlvaZ <carlos.alvarenga017@gmail.com>
2022-09-09 00:12:25 -0600

    Primer version funcional de la funcion mostrar()

--- Ejercicio_1/matriz.js
+++ Ejercicio_1/matriz.js
@@ -17,13 +17,22 @@ generate.addEventListener('click', ()=>{
         return
     }
 
-    console.log(crear_matriz(inDimensions.value, inElements.value))
+    // creando una nueva matriz usando crear_matriz()
+    let new_matrix = crear_matriz(inDimensions.value, inElements.value)
+
+    // creando un fragmento de html con todo el contenido de la matriz creada anteriormente
+    let fragment = mostrar(new_matrix)
+    
+    // limpiando y anadiendo el fragmento de html a la tabla
+    table.innerHTML = ''
+    table.appendChild(fragment)
 
 })
 
 
 // Creando funcion crear_matriz que se vale de ciclos for para crear una matriz multidimensional
 function crear_matriz(dimensiones, elementos){
+
     let matriz = []
     for(let i = 1; i <= dimensiones; i++){
         let aux = []
@@ -34,4 +43,40 @@ function crear_matriz(dimensiones, elementos){
         matriz.push(aux)
     }
     return matriz
+}
+
+// Creando funcion para mostrar los elementos de la matriz
+function mostrar(arreglo, target){
+    let docFragment = new DocumentFragment()
+    for(fila in arreglo){
+        let tr = document.createElement('tr')
+        for(columna in arreglo[fila]){
+            let td = document.createElement('td')
+            let text = document.createTextNode(arreglo[fila][columna])
+            td.appendChild(text)
+            tr.appendChild(td)
+        }
+        docFragment.appendChild(tr)
+    }
+    return docFragment
+}
+
+function min_max(numero, indice){
+    let minimo = 0
+    let maximo = 0
+    let indice = [0, 0]
+    if(numero == 1){
+        minimo = numero
+        min_indice = indice
+    }
+    else{
+        if(numero < minimo){
+            minimo = numero
+            min_indice = indice
+        }
+        else if(numero > maximo){
+            maximo = numero
+            max_indice = indice
+        }
+    }
 }
